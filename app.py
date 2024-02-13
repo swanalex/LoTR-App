e177f82e4dcb062c54a3ca66bac968f1a61ad442
Alex Swanson <86210924+swanalex@users.noreply.github.com>
2024-02-12 19:10:12 -0500

    Update app.py
    Moved API calls to https://the-one-api.dev/ outside of the POST request from button clicks

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import requests
-# import random
+import random
 import os
 from dotenv import load_dotenv
 import zmq
@@ -16,43 +16,61 @@ BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
 # ZeroMQ setup
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
+# change to socket # of partner's micro***
 socket.connect("tcp://127.0.0.1:5555")
 
 
 def generate_random_numbers():
     socket.send_string("generate_numbers")
     response = socket.recv_json()
-    return response["num1"], response["page_num"], response["num2"]
+    return response["num1"], response["num2"]
 
 
-# Initialize chars_dict and  movies_dict
+# Initialize chars_dict, movies_dict, and quotes_list
 chars_dict = {}
 movies_dict = {}
+quotes_list = []
 
 
 def initialize_dicts():
-    global chars_dict, movies_dict
+    global chars_dict, movies_dict, quotes_list, chars_list
     api_endpoint1 = 'https://the-one-api.dev/v2/character'
     api_endpoint_movies = 'https://the-one-api.dev/v2/movie'
     headers = {'Authorization': f'Bearer {BEARER_TOKEN}'}
 
-    # Initialize chars_dict
+    # Compile chars_dict {'Char_name1': '_charID1', etc..}
     response1 = requests.get(api_endpoint1, headers=headers)
     data1 = response1.json()
-    chars_dict = {}
+    chars_list = data1['docs']
     for i in range(933):
         char_obj = data1['docs'][i]
         specific_char = data1['docs'][i]['name']
         chars_dict[specific_char] = char_obj['_id']
 
-    # Initialize movies_dict
-    response3 = requests.get(api_endpoint_movies, headers=headers)
-    data3 = response3.json()
+    # Compile movies_dict {'Movie_name1': '_movieID1', etc..}
+    response2 = requests.get(api_endpoint_movies, headers=headers)
+    data2 = response2.json()
     movies_dict = {}
     for i in range(2, 8):
-        movie = data3['docs'][i]
-        specific_movie = data3['docs'][i]['name']
+        movie = data2['docs'][i]
+        specific_movie = data2['docs'][i]['name']
         movies_dict[specific_movie] = movie['_id']
+
+    # Initialize quotes_list (list of quote objects)
+    response3_page1 = requests.get('https://the-one-api.dev/v2/quote?page=1',
+                                   headers=headers)
+    response3_page2 = requests.get('https://the-one-api.dev/v2/quote?page=2',
+                                   headers=headers)
+    response3_page3 = requests.get('https://the-one-api.dev/v2/quote?page=3',
+                                   headers=headers)
+
+    data3_page1 = response3_page1.json()
+    data3_page2 = response3_page2.json()
+    data3_page3 = response3_page3.json()
+
+    quotes_list = data3_page1['docs']
+    quotes_list += data3_page2['docs']
+    quotes_list += data3_page3['docs']
 
 
 # Call the function to initialize both dictionaries when the app starts
@@ -66,31 +84,18 @@ def home():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global chars_dict, movies_dict
+    global chars_dict, movies_dict, quotes_list, chars_list
 
-    data1, data2 = None, None
     cleared = ''
 
     if request.method == 'POST':
-        num1, page_num, num2 = generate_random_numbers()
-        # num1 = random.randint(1, 933)
-        # page_num = random.randint(1, 3)
-        # num2 = random.randint(1, 1000)
-        # if page_num == 3:
-        #     num2 = random.randint(1, 384)
+        # num1, page_num, num2 = generate_random_numbers()
+        num1 = random.randint(1, 933)
+        num2 = random.randint(1, 2384)
 
-        api_endpoint1 = 'https://the-one-api.dev/v2/character'
-        api_endpoint2 = 'https://the-one-api.dev/v2/quote?page={page_num}'
-
-        headers = {'Authorization': f'Bearer {BEARER_TOKEN}'}
-        response1 = requests.get(api_endpoint1, headers=headers)
-        data1 = response1.json()
-
-        response2 = requests.get(api_endpoint2, headers=headers)
-        data2 = response2.json()
-
-        # getting character info
-        character = data1['docs'][num1]
+        # Getting character info
+        # Take care of fields that have 'NaN' or '' to read 'Unknown'
+        character = chars_list[num1]
         name = character['name']
         if name == 'NaN' or name == '':
             name = 'Unknown'
@@ -108,9 +113,9 @@ def index():
             death = 'Unknown or still living'
         char_string = f'Name: {name}, Race: {race}, Gender: {gender}, Birth: {birth}, Death: {death}'
 
-        # Display movie name and character name for quote
-        # instead of movie_id and char_id
-        quote = data2['docs'][num2]
+        # Use the global chars_dict and movies_dict to display movie name
+        # and character name for each quote, instead of movie_id and char_id
+        quote = quotes_list[num2]
         dialogue = quote['dialog']
         movie = quote['movie']
         for name, id in movies_dict.items():
@@ -126,7 +131,6 @@ def index():
         if button_value == 'get_char':
             return render_template('index.html', text=char_string)
         if button_value == 'get_quote':
-            print(movies_dict)
             return render_template('index.html', text=quote_string)
         else:
             return render_template('index.html', text=cleared)
