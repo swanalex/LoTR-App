78dd85cf9b243030a0cabb55eb62c17fd6ed63c7
Alex Swanson <86210924+swanalex@users.noreply.github.com>
2024-03-11 18:17:46 -0400

    Update get_pic.py

--- get_pic.py
+++ get_pic.py
@@ -1,35 +1,9 @@
+import os
+
+
 def get_pic(character):
-    if character == 'Frodo Baggins':
-        return './images/frodo.jpg'
-    elif character == 'Meriadoc Brandybuck':
-        return './images/merry.jpg'
-    elif character == 'Samwise Gamgee':
-        return './images/sam.jpg'
-    elif character == 'Peregrin Took':
-        return './images/pippin.jpg'
-    elif character == 'Gandalf':
-        return './images/gandalf.jpg'
-    elif character == 'Aragorn II Elessar':
-        return './images/aragorn.jpg'
-    elif character == 'Legolas':
-        return './images/legolas.jpg'
-    elif character == 'Gollum':
-        return './images/gollum.jpg'
-    elif character == 'Elrond':
-        return './images/elrond.jpg'
-    elif character == 'Faramir':
-        return './images/faramir.jpg'
-    elif character == 'Saruman':
-        return './images/saruman.jpg'
-    elif character == 'Gimli':
-        return './images/gimli.jpg'
-    elif character == 'Boromir':
-        return './images/boromir.jpg'
-    elif character == 'Éowyn':
-        return './images/eowyn.jpg'
-    elif character == 'Bilbo Baggins':
-        return './images/bilbo.jpg'
-    elif character == 'Arwen':
-        return './images/arwen.jpg'
-    else:
-        return False
+    image_folder = 'C:/Users/16316/Desktop/OSU/CS 361/Simple_Flask_App/static/images'
+    for image in os.listdir(image_folder):
+        if image == character + '.jpg':
+            return f'./images/{character}.jpg'
+    return False
