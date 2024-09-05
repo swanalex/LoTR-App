69525528c58c000ef72a1b6e1e0ca32bd0dd5ce1
Alex Swanson <86210924+swanalex@users.noreply.github.com>
2024-09-04 23:23:43 -0400

    Update path to IMAGE_FOLDER in get_pic.py

--- get_pic.py
+++ get_pic.py
@@ -6,7 +6,7 @@ def get_pic(character):
 
     load_dotenv()
     # replace with your own image folder location
-    IMAGE_FOLDER = os.environ.get('IMAGE_FOLDER')
+    IMAGE_FOLDER = os.path.join('static', 'images')
     
 
     for image in os.listdir(IMAGE_FOLDER):
